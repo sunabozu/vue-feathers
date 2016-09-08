32a9f4dbe363350a281de00624caf46c4cb197d5
troorl <troorl@gmail.com>
2016-09-08 16:33:16 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# vue-feathers
+
+> Integration with the Feathers framework for Vue.js
+
+**NOTE:** It's supposed to be compatible both with Vue 1.x and 2.x. It requires IE9+ or Safari 5+
+
+### Install
+
+``` bash
+npm install vue-feathers --save
+```
+
+### Usage
+
+``` js
+// Include it as a CommonJS module
+var Vue = require('vue')
+var vueFeathers = require('vue-feathers')
+
+// And plug it in
+Vue.use(vueFeathers)
+```
+
+Now in every component you get a new property called `$services`, which allows you to interact with all of your Feathers services:
+
+``` js
+this.$services.messages.find()
+
+// or
+
+this.$services.messages.create(...)
+```
+
+To subscribe on the [events](http://docs.feathersjs.com/real-time/events.html) your services generate, you just need to use a separate `feathers` section in your component:
+
+``` js
+export default {
+  data() {
+    return {
+      ...
+    }
+  },
+
+  methods: {
+    ...
+  }
+
+  feathers: { // here is our section
+    messages: { // here is the subsection for the 'messages' service
+      created(data) {
+        ...
+      },
+
+      updated(data) {
+        ...
+      }
+    }
+  }
+}
+```
+
+Vue-feathers does all the clean up before your component is destroyed (using the `removeListener` function).
+
+In case you need to do something more complex, there is a `$feathers` property for that:
+
+``` js
+this.$feathers.service('messages').on(...)
+```
+
+### License
+
+[MIT](http://opensource.org/licenses/MIT)
---
+++ vue-feathers.js
@@ -0,0 +1,59 @@
+(function() {
+  'use strict'
+
+  var feathers = require('feathers/client')
+  var socketio = require('feathers-socketio/client')
+  var io = require('socket.io-client')
+
+  var socket = io()
+  var server = feathers().configure(socketio(socket))
+
+  var plugin = {
+    install: function(Vue) {
+      // Every component will have this
+      Vue.mixin({
+        created: function() {
+          this.$feathers = server
+
+          // Setting up the services
+          this.$services = server.services
+
+          if(!this.$options.feathers)
+            return
+
+          // Setting up the events
+          var keys = Object.keys(this.$options.feathers) // services, IE9+
+          for(var i = 0; i < keys.length; i++) {
+            var service = this.$options.feathers[keys[i]]
+            var ekeys = Object.keys(service) // handlers
+
+            for(var k = 0; k < ekeys.length; k++) {
+              server.service(keys[i]).on(ekeys[k], service[ekeys[k]].bind(this))
+            }
+          }
+        },
+
+        beforeDestroy: function() {
+          // Removing the events
+          var keys = Object.keys(this.$options.feathers) // services, IE9+
+          for(var i = 0; i < keys.length; i++) {
+            var service = this.$options.feathers[keys[i]]
+            var ekeys = Object.keys(service) // handlers
+
+            for(var k = 0; k < ekeys.length; k++) {
+              server.service(keys[i]).removeListener(ekeys[k], service[ekeys[k]].bind(this))
+            }
+          }
+        }
+      })
+    }
+  }
+
+  if(typeof exports === 'object' && typeof module === 'object') {
+    module.exports = plugin
+  } else if(typeof define === 'function' && define.amd) {
+    define(function () { return plugin })
+  } else if (typeof window !== 'undefined') {
+    window.VueAsyncData = plugin
+  }
+})()
